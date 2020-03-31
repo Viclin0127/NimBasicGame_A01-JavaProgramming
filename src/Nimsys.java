40a80af5c4909d34d74c57b65273245fd6efcc73
YuanHungLin <viclin@linyuanhongde-MacBook-Pro.local>
2020-03-31 21:32:29 +1100

    add authorship

--- src/Nimsys.java
+++ src/Nimsys.java
@@ -1,35 +1,45 @@
+/**
+ * COMP90041 Programming and Software Developing Assignment 1:
+ * Nim is a two-player game that the first one who remove the last one stone will lose
+ *
+ * @author Yuan
+ * Name: Yuan Hung Lin
+ * StudentID: 1119147
+ * Username: yuanhungl
+ */
+
 import java.util.Scanner;
 
 public class Nimsys {
 
-    public static String again = "Y";
+    public static String playGame = "Y";
 
     public static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        //** welcome sentence.
+        // welcome sentence.
         System.out.println("Welcome to Nim\n");
 
-        //** enter player1's name, and create variable NimPlayer
+        // enter player1's name, and create variable NimPlayer
         System.out.println("Please enter Player 1's name:");
         NimPlayer player1 = new NimPlayer();
         player1.name = scan.next();
 
-        //** enter player2's name, and create variable NimPlayer
+        // enter player2's name, and create variable NimPlayer
         System.out.println("\nPlease enter Player 2's name:");
         NimPlayer player2 = new NimPlayer();
         player2.name = scan.next();
         System.out.println();
 
-        //** create a loop for playing game again or not
-        while (again.equals("Y")) {
+        // create a loop for playing game again or not
+        while (playGame.equals("Y")) {
 
-            //** create upper bound of stone removal
+            // create upper bound of stone removal
             System.out.println("Please enter upper bound of stone removal:");
             int upBond = scan.nextInt();
 
-            //** create total stones
+            // create total stones
             System.out.println("\nPlease enter initial number of stones:");
             int totalStones = scan.nextInt();
 
@@ -38,17 +48,19 @@ public class Nimsys {
 
             // recursively play game in turns
             while (totalStones != 0) {
-                checkStones(totalStones);
+                checkStones(totalStones);       // print the rest of stones
                 turns++;
-                if ((turns % 2) != 0) {
+                if ((turns % 2) != 0) {         // if turns is odd, it's player1's turn
                     int out1 = player1.removeStone();
                     totalStones -= out1;
-                } else {
+                } else {                        // if turns is even, it's player2's turn
                     int out2 = player2.removeStone();
                     totalStones -= out2;
                 }
             }
             System.out.println("\nGame Over");
+
+            // print the winner
             if ((turns % 2) != 0) {
                 String winner2 = player2.name + " wins!\n";
                 System.out.println(winner2);
@@ -58,11 +70,15 @@ public class Nimsys {
             }
 
             System.out.print("Do you want to play again (Y/N):\n");
-            again = scan.next();
+            playGame = scan.next();
         }
     }
 
-
+    /**
+     * func to print the total stones
+     *
+     * @param totalStones
+     */
     public static void checkStones(int totalStones) {
         System.out.printf("\n%d stones left:", totalStones);
         for (int i = 0; i < totalStones; i++) {
